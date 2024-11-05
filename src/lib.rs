71d15b86380444b043b82e7417c1bac2784192be
imnotzrus <tu0qnqy3n@gmail.com>
2024-11-05 21:59:35 +0700

    feat: impl webhook

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 #![cfg_attr(debug_assertions, allow(warnings))]
 
+use std::env;
+use std::sync::Arc;
+
 use teloxide::adaptors::DefaultParseMode;
 use teloxide::Bot as TBot;
 use teloxide::dispatching::{Dispatcher, HandlerExt, UpdateFilterExt};
 use teloxide::dptree;
 use teloxide::requests::RequesterExt;
 use teloxide::types::{ParseMode, Update};
+use teloxide::update_listeners::webhooks;
 
 use crate::command::Command;
 use crate::storage::Storage;
@@ -24,7 +28,21 @@ pub fn setup_logger() {
 }
 
 pub async fn spin_up() {
+  let inbound = env::var("INBOUND")
+    .expect("Missing `INBOUND` address config")
+    .parse()
+    .expect("Unable to parse `INBOUND` from string");
+  let outbound = env::var("OUTBOUND")
+    .expect("Missing `OUTBOUND` address config")
+    .parse()
+    .expect("Unable to parse `INBOUND` from string");
+
   let bot = TBot::from_env().parse_mode(ParseMode::MarkdownV2);
+  let options = webhooks::Options::new(inbound, outbound);
+  let listener = webhooks::axum(Clone::clone(&bot), options)
+    .await
+    .expect("Unable to build listener");
+
   let storage = Storage::default();
   let handler = dptree::entry()
     .branch(
@@ -39,10 +57,11 @@ pub async fn spin_up() {
     .branch(
       Update::filter_callback_query().endpoint(handler::callback::handle),
     );
+
   Dispatcher::builder(bot, handler)
     .dependencies(dptree::deps![storage])
     .enable_ctrlc_handler()
     .build()
-    .dispatch()
+    .dispatch_with_listener(listener, Arc::new(|_| async {}))
     .await;
 }
